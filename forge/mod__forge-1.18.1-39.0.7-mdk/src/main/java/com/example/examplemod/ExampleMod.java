0da1c591136773f5ae7c188326b2fa02585838d7
y² <yy@yuyuan.org>
2021-12-22 01:20:23 -0800

    1.18.1 + https://youtu.be/3Uqnb8TqjTY?t=1089

--- forge/mod__forge-1.18.1-39.0.7-mdk/src/main/java/com/example/examplemod/ExampleMod.java
+++ forge/mod__forge-1.18.1-39.0.7-mdk/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,5 +1,6 @@
 package com.example.examplemod;
 
+import com.example.examplemod.setup.Registration;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -18,13 +19,17 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod(ExampleMod.MOD_ID)
 public class ExampleMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
+    public static final String MOD_ID = "examplemod";
+
     public ExampleMod() {
+        Registration.register();
+
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -61,16 +66,5 @@ public class ExampleMod
     public void onServerStarting(ServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
     }
 }
---
+++ forge/mod__forge-1.18.1-39.0.7-mdk/src/main/java/com/example/examplemod/setup/ModItems.java
@@ -0,0 +1,14 @@
+package com.example.examplemod.setup;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final RegistryObject<Item> SILVER_INGOT = Registration.ITEMS.register("silver_ingot", () ->
+            new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS))
+    );
+
+    static void register() {}
+}
